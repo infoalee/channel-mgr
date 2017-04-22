071442387175d79bdbc47bc01dc8da454465d752
infoalee <infoalee@Alees-MacBook-Pro.local>
2017-04-22 20:51:21 +0700

    edit dashboard to Function fetch from Database

--- _view/dashboard.php
+++ _view/dashboard.php
@@ -1,5 +1,17 @@
 <script src="./_controller/dashboard.js"></script>
-<div id="page-wrapper">
+<?
+include("../class.php");
+$dashboard = new Dashboard;
+//createMenuItem($pnStyle, $icon, $label, $detail, $notification, $href, $contentID)
+//**** New class database ****//
+$strHost = "127.0.0.1";
+$strDB = "ci_channel_mgt";
+$strUser = "root";
+$strPassword = "p@ssw0rd";
+$clsMyDB = new MySQLDatabase($strHost,$strDB,$strUser,$strPassword);
+
+?>
+    <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">Dashboard</h1>
@@ -8,188 +20,62 @@
             </div>
             <!-- /.row -->
             <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-calendar-plus-o fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">26</div>
-                                    <div>Booking!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#/booking" id="lnkBooking">
-                            <div class="panel-footer">
-                                <span class="pull-left">Booking Here</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-green">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-calendar-check-o fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">12</div>
-                                    <div>Comfrimed Slot!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#/myslot">
-                            <div class="panel-footer">
-                                <span class="pull-left">Check my slot</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-cogs fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">124</div>
-                                    <div>Manage Booking!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Check Here</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-calendar-times-o fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>Reject Slot!</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Check Here</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-peterriver">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-commenting fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>SMS Slot</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Check Here</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-alizarin">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-envelope-o fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>Email Slot</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Check Here</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-emerald">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <!-- <i class="fa fa-mobile fa-5x"></i>-->
-                                   <span class="glyphicon glyphicon-phone fa-5x"></span>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>KMB Slot</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left ">Check Here</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right panel-success"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+<?
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-wetasphalt">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-server fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>Function Control</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left ">Check Here</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right panel-success"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>                    
+//**** Call to function select record ****//
+$clsMyDB->strTable = "MENU_DASHBOARD";
+$clsMyDB->strCondition = " ID > 0 ";
+$objSelect = $clsMyDB->fncReturnResult();
+
+    while($row = @mysql_fetch_array($objSelect)){
+        $_pnStyle   = $row['PN_STYLE'];
+        $_icon      = $row['PN_ICON'];
+        $_label     = $row['NAME'];
+        $_detail    = $row['CLICK_NAME'];
+        $_notification = rand(1,100);
+        $_href      = $row['HREF'];
+        $_contentID = $row['CONTENT_ID'];
+
+        $elm = $dashboard -> createMenuItem($_pnStyle, $_icon, $_label, $_detail, $_notification, $_href, $_contentID);
+        echo $elm;
+    }
+
+            
+?> 
 
             </div>
             <!-- /.row -->
            
         </div>
         <!-- /#page-wrapper -->
+<?
+Class Dashboard {
+
+    function createMenuItem($pnStyle, $icon, $label, $detail, $notification, $href, $contentID){
+        $element =  "<div class='col-lg-3 col-md-6'>
+                    <div class='panel " . $pnStyle . "'>
+                        <div class='panel-heading'>
+                            <div class='row'>
+                                <div class='col-xs-3'>
+                                    <i class='fa " .$icon . " fa-5x'></i>
+                                </div>
+                                <div class='col-xs-9 text-right'>
+                                    <div class='huge'>" . $notification . "</div>
+                                    <div>" . $label . "</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href='" . $href . "' id='" . $contentID ."'>
+                            <div class='panel-footer'>
+                                <span class='pull-left'>" . $detail . "</span>
+                                <span class='pull-right'><i class='fa fa-arrow-circle-right'></i></span>
+                                <div class='clearfix'></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>";
+                return $element;
+    }
+}
+?>
--- class.php
+++ class.php
@@ -105,62 +105,116 @@ class ODBC
 { 
  var $handle; 
   
- // bool connect(string $dsn, string $user, stirng $pass) 
- function connect($dsn, $user, $pass) 
- { 
-   $this->handle = odbc_connect($dsn, $user, $pass); 
-   if (!$this->handle) 
-     return false; 
-   return true; 
- } 
-  
- // resourceid query(string $sql) 
- function query($sql) 
- { 
-   $rs = @odbc_exec($this->handle, $sql); 
-       if ($rs) 
-         { 
-return $rs; 
-       } 
-         else 
-       { 
-$sErr = "<b>Error:</b> " . $this->getErrorMsgs() . "<br>\n"; 
-$sErr .= "<b>SQL:</b> " . $sql; 
-die($sErr); 
+    // bool connect(string $dsn, string $user, stirng $pass) 
+    function connect($dsn, $user, $pass) 
+    { 
+    $this->handle = odbc_connect($dsn, $user, $pass); 
+        if (!$this->handle) 
+            return false; 
+        return true; 
+    } 
+    
+    // resourceid query(string $sql) 
+    function query($sql) 
+    { 
+        $rs = @odbc_exec($this->handle, $sql); 
+            if ($rs) 
+                { 
+                return $rs; 
+            } 
+            else 
+            { 
+            $sErr = "<b>Error:</b> " . $this->getErrorMsgs() . "<br>\n"; 
+            $sErr .= "<b>SQL:</b> " . $sql; 
+            die($sErr); 
+        } 
+    } 
+
+    // string getErrorMsgs() 
+    function getErrorMsgs() 
+    { 
+            return odbc_errormsg($this->handle); 
+    } 
+
+        // void disconnect() 
+    function disconnect() 
+    { 
+        if ($this->handle) 
+        odbc_close($this->handle); 
+    } 
+
+    // int Rows()
+    function getRows($sql){
+    $num=odbc_num_rows($this->query($sql));
+        if ($num) 
+                { 
+        return $num; 
+            } 
+                else 
+            { 
+            $sErr = "<b>Error:</b> " . $this->getErrorMsgs() . "<br>\n"; 
+            $sErr .= "<b>SQL:</b> " . $sql; 
+            die($sErr); 
+        } 
+
+    }
+
+
 } 
- } 
 
- // string getErrorMsgs() 
-function getErrorMsgs() 
-{ 
-return odbc_errormsg($this->handle); 
-} 
+	/**** Class Database ****/
+	Class MySQLDatabase
+	{
+		/**** function connect to database ****/
+		function MySQLDatabase($strHost,$strDB,$strUser,$strPassword)
+		{
+				$this->objConnect = mysql_connect($strHost,$strUser,$strPassword);
+				$this->DB = mysql_select_db($strDB);
+		}
 
-// void disconnect() 
- function disconnect() 
- { 
-   if ($this->handle) 
-     odbc_close($this->handle); 
- } 
+		/**** function insert record ****/
+		function fncInsertRecord()
+		{
+				$strSQL = "INSERT INTO $this->strTable ($this->strField) VALUES ($this->strValue) ";
+				return mysql_query($strSQL);
+		}
 
-// int Rows()
-function getRows($sql){
-$num=odbc_num_rows($this->query($sql));
-   if ($num) 
-         { 
-return $num; 
-       } 
-         else 
-       { 
-$sErr = "<b>Error:</b> " . $this->getErrorMsgs() . "<br>\n"; 
-$sErr .= "<b>SQL:</b> " . $sql; 
-die($sErr); 
-} 
+        
+        function fncReturnResult()
+		{
+				$strSQL = "SELECT * FROM $this->strTable WHERE $this->strCondition ";
+				$objQuery = @mysql_query($strSQL);
+				return $objQuery;
+		}
 
-}
+		/**** function select record ****/
+		function fncSelectRecord()
+		{
+				$strSQL = "SELECT * FROM $this->strTable WHERE $this->strCondition ";
+				$objQuery = @mysql_query($strSQL);
+				return @mysql_fetch_array($objQuery);
+		}
+        
+		/**** function update record (argument) ****/
+		function fncUpdateRecord($strTable,$strCommand,$strCondition)
+		{
+				$strSQL = "UPDATE $strTable SET  $strCommand WHERE $strCondition ";
+				return @mysql_query($strSQL);
+		}
 
+		/**** function delete record ****/
+		function fncDeleteRecord()
+		{
+				$strSQL = "DELETE FROM $this->strTable WHERE $this->strCondition ";
+				return @mysql_query($strSQL);
+		}
 
-} 
+		/*** end class auto disconnect ***/
+		function __destruct() {
+				return @mysql_close($this->objConnect);
+	    }
+	}			
+
 
 
 ?>
@@ -177,7 +231,7 @@ original function of the PHP.
 The function is tested using PHP 5.1.4 in Windows XP 
 and Apache WebServer. 
 */ 
-function money_format($format, $number) 
+function money_formats($format, $number) 
 { 
     $regex  = '/%((?:[\^!\-]|\+|\(|\=.)*)([0-9]+)?'. 
               '(?:#([0-9]+))?(?:\.([0-9]+))?([in%])/'; 
@@ -262,7 +316,7 @@ function money_format($format, $number)
         $format = str_replace($fmatch[0], $value, $format); 
     } 
     return $format; 
-} 
+}
 
 ?>
 
