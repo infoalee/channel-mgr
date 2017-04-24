f55c303507b53d907f76152167d2936052656975
Alee Waedureh <alee.w@kasikornbank.com>
2017-04-24 18:28:01 +0700

    update landing_page.js

--- _controller/landing_page.js
+++ _controller/landing_page.js
@@ -1,29 +1,29 @@
- $(function() {
-     $('a').click(function() {
-         updateLinkActive(this.href);
-         if (this.hash == '#/booking') {
-             show_booking();
-         } else if (this.hash == '#/dashboard') {
-             show_dashboard();
-         } else if (this.hash == '#') {
-             show_dashboard();
-         }
-     });
+$(function() {
 
- });
+    if ($(location).attr('hash').length < 1 || $(location).attr('hash') == null) {
+        show_dashboard();
+    } else if ($(location).attr('hash') == '#/booking') {
+        show_booking();
+    } else if ($(location).attr('hash') == '#/dashboard') {
+        show_dashboard();
+    } else if ($(location).attr('hash') == '#/calendar') {
+        show_calendar();
+    } else {
+        show_dashboard();
+    }
 
- $(function() {
+});
 
-     if ($(location).attr('hash').length < 1 || $(location).attr('hash') == null) {
-         show_dashboard();
-     } else if ($(location).attr('hash') == '#/booking') {
-         show_booking();
-     } else if ($(location).attr('hash') == '#/dashboard') {
-         show_dashboard();
-     } else if ($(location).attr('hash') == '#/calendar') {
-         show_calendar();
-     } else {
-         show_dashboard();
-     }
+$(function() {
+    $('a').click(function() {
+        updateLinkActive(this.href);
+        if (this.hash == '#/booking') {
+            show_booking();
+        } else if (this.hash == '#/dashboard') {
+            show_dashboard();
+        } else if (this.hash == '#') {
+            show_dashboard();
+        }
+    });
 
- });
+});
