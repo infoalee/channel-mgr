7035ea8967986ac78aba9e7425d4f669d88e429c
infoalee <infoalee@gmail.com>
2017-06-23 16:20:24 +0700

    Add files via upload

---
+++ NewTaskP1-1.php
@@ -0,0 +1,121 @@
+<<<<<<< HEAD
+
+<!-- /#page-wrapper -->
+<div id="page-wrapper">
+
+
+          <div class="row">
+              <div class="col-lg-12">
+                  <h1 class="page-header">New Task (AO Role)</h1>
+              </div>
+              <!-- /.col-lg-30 -->
+          </div>
+
+
+<div class="row s-control">
+   <div class="form-group">
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-3">
+     <label class="control-label inline">Campaign Code :</label>
+     </div>
+                                         
+      <p class="help-block"></p>
+     </div>
+     </div>
+
+
+<div class="row s-control">
+   <div class="form-group">
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-3">
+     <label class="control-label inline">Campaign Name :</label>
+     </div>
+                                         
+      <p class="help-block"></p>
+     </div>
+     </div>
+
+
+	 	 <div class="row s-control">
+   <div class="form-group">
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-3">
+     <label class="control-label inline">Contact Date :</label>
+     </div>
+	 	<div class="col-md-8">
+     <label class="control-label inline">Amount :</label>
+     </div>
+                                         
+      <p class="help-block"></p>
+     </div>
+     </div>
+
+
+	 <div class="row s-control">
+   <div class="form-group">
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-3">
+     <label class="control-label inline">Channel : SMS</label>
+     </div>
+        	<div class="col-md-3">
+     <label class="control-label inline">Type : Normal (N)</label>
+     </div>                                 
+      <p class="help-block"></p>
+     </div>
+     </div>
+
+                                         
+<!-- Row -->
+<div class="row s-control">
+   <div class="form-group">
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-2">
+     <label class="control-label inline">SMS Message 1 :</label>
+     </div>
+	 <span class="col-md-5 text-left">
+ <textarea name="Message"  required="required" class="form-control inline" id="Message" placeholder="Message." data-validation-required-message="Please enter to Search"> </textarea>
+                                            </span>
+
+<!-- Row -->
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-2">
+<button class="btn btn-md btn-success" id="btnSubmit" type="submit"><i class="fa fa-plus" aria-hidden="true"></i> Add</button>
+                                         
+      <p class="help-block"></p>
+     </div>
+    </div>
+</div>
+
+<br>
+
+<!-- Row -->
+<div class="row s-control">
+   <div class="form-group">
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-2">
+     <label class="control-label inline">SMS Message 2 :</label>
+     </div>
+	 <span class="col-md-5 text-left">
+ <textarea name="Message"  required="required" class="form-control inline" id="Message" placeholder="Message." data-validation-required-message="Please enter to Search"> </textarea>
+                                            </span>
+
+
+   <div class="col-md-1">
+    </div>
+	<div class="col-md-2">
+
+                                         
+      <p class="help-block"></p>
+     </div>
+    </div>
+</div>
+<!-- Row -->
+
+</div>
